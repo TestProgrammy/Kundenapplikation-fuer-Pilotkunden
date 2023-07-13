c5527efd6883879cd4b92b028b44850b374038c9
TestProgrammy <xedusold@outlook.de>
2023-07-13 16:51:09 +0200

    sadlksad

--- project/app/src/test/java/de.ewe.tk.customer/ValidatorTest.java
+++ project/app/src/test/java/de.ewe.tk.customer/ValidatorTest.java
@@ -38,7 +38,7 @@ public class ValidatorTest {
     @Test
     public void testPostcodeValidation() {
         // Positive Fälle
-        String[] pIntValues = { "01234", "01912", "12092" };
+        String[] pIntValues = { "01234 ", " 01912", "12092" };
         for (String value : pIntValues) {
             assertTrue(Validator.validatePostcode(value));
         }
@@ -105,7 +105,7 @@ public class ValidatorTest {
         }
 
         // Negative Fälle
-        String[] nStringValues = { "", "?", "-", "j", "1", " 1", "ja", "nein", "nmae", " ", " nA", "asdr", "[", "aS" };
+        String[] nStringValues = { "", "?", "-", "j", "1", "ja", "nein", "nmae", " ", "asdr", "[", "aS" };
         for (String value : nStringValues) {
             assertFalse(Validator.validateLastName(value));
         }
@@ -166,7 +166,11 @@ public class ValidatorTest {
         for (String value : pStringValues) {
             assertTrue(Validator.validatePhoneNumber(value));
         }
-
+        /*
+         * 0231 274870
+         * 0231 27487-03
+         * +49 30 27012244
+         */
         // Negative Fälle
         String[] nStringValues = { "", "?", "-", "j", "1", "ja", "nein", "Nmae", " ", " Na", "asdr", "[", "aS",
                 "016/130671", "06/3074744", "9163/131067", "0156/307474444" };
@@ -183,7 +187,10 @@ public class ValidatorTest {
         for (String value : pStringValues) {
             assertTrue(Validator.validateMobileNumber(value));
         }
-
+        /*
+         * 0170 7654321
+         * +49 170 7654321
+         */
         // Negative Fälle
         String[] nStringValues = { "", "?", "-", "j", "1", "ja", "nein", "Nmae", " ", " Na", "asdr", "[", "aS",
                 "016/1310671", "9156/3074744", "0163/131067", "0156/30747444" };
@@ -200,7 +207,32 @@ public class ValidatorTest {
         for (String value : pStringValues) {
             assertTrue(Validator.validateFax(value));
         }
-
+        /*
+         * +(Landesvorwahl)(Ortsvorwahl)(Faxnummer)
+         * 
+         * z.B.: +49-711-9876543
+         * oder 0049(711)1234567
+         * Beispiele für den Aufbau einer Faxnummer
+         * Faxnummer Beispiel 1:
+         * 
+         * In den InterFAX Account-Einstellungen ist hinterlegt: a) Länderkennung:
+         * Deutschland, b) Ortsvorwahl: 711 (für Stuttgart)
+         * Es soll ein Fax in ein anderes Land gesendet werden (z.B. Großbritannien, mit
+         * der Länderkennzahl '44'):
+         * Option 1: +44 161 999 8888
+         * Option 2: 0044 (161) 999 8888
+         * 
+         * Es soll ein Fax in eine andere Stadt innerhalb Deutschlands gesendet werden:
+         * Option 1: 040-999 8888
+         * Option 2: +49 (40) 999 8888
+         * 
+         * Es soll ein Fax an eine Faxnummer in Stuttgart gesendet werden
+         * Option 1: 5222 8888
+         * Option 2: 0711-5222 8888
+         * Option 3: +49 (711) 5222 8888
+         * 
+         * 0211 2147-36
+         */
         // Negative Fälle
         String[] nStringValues = { "", "?", "-", "j", "1", "ja", "nein", "Nmae", " ", " Na", "asdr", "[", "aS",
                 "155/74743421", "06563/750276483", " 09/957224", "06237/21060 " };
@@ -213,14 +245,66 @@ public class ValidatorTest {
     public void testEmailValidation() {
         // Positive Fälle
         String[] pStringValues = { "welsch@mail.xyz", "r.radtke@mail.xyz", "b.brauner@mail.xyz", "n.hacker@mail.xyz",
-                " frederic.schleicher@mail.de", "y.behrend@mail.com ", "Lilian.kahl@yahoo.xyz" };
+                " frederic.schleicher@mail.de", "y.behrend@mail.com ", "Lilian.kahl@yahoo.xyz",
+                "frederic-schleicher@mail.de" };
         for (String value : pStringValues) {
             assertTrue(Validator.validateEmail(value));
         }
-
+        /*
+         * Examples
+         * 
+         * Valid email addresses
+         * 
+         * simple@example.com
+         * very.common@example.com
+         * disposable.style.email.with+symbol@example.com
+         * other.email-with-hyphen@and.subdomains.example.com
+         * fully-qualified-domain@example.com
+         * user.name+tag+sorting@example.com (may go to user.name@example.com inbox
+         * depending on mail server)
+         * x@example.com (one-letter local-part)
+         * example-indeed@strange-example.com
+         * test/test@test.com (slashes are a printable character, and allowed)
+         * admin@mailserver1 (local domain name with no TLD, although ICANN highly
+         * discourages dotless email addresses[29])
+         * example@s.example (see the List of Internet top-level domains)
+         * " "@example.org (space between the quotes)
+         * "john..doe"@example.org (quoted double dot)
+         * mailhost!username@example.org (bangified host route used for uucp mailers)
+         * "very.(),:;<>[]\".VERY.\"very@\\ \"very\".unusual"@strange.example.com
+         * (include non-letters character AND multiple at sign, the first one being
+         * double quoted)
+         * user%example.com@example.org (% escaped mail route to user@example.com via
+         * example.org)
+         * user-@example.org (local-part ending with non-alphanumeric character from the
+         * list of allowed printable characters)
+         * postmaster@[123.123.123.123] (IP addresses are allowed instead of domains
+         * when in square brackets, but strongly discouraged)
+         * postmaster@[IPv6:2001:0db8:85a3:0000:0000:8a2e:0370:7334] (IPv6 uses a
+         * different syntax)
+         * 
+         * Invalid email addresses
+         * 
+         * Abc.example.com (no @ character)
+         * A@b@c@example.com (only one @ is allowed outside quotation marks)
+         * a"b(c)d,e:f;g<h>i[j\k]l@example.com (none of the special characters in this
+         * local-part are allowed outside quotation marks)
+         * just"not"right@example.com (quoted strings must be dot separated or the only
+         * element making up the local-part)
+         * this is"not\allowed@example.com (spaces, quotes, and backslashes may only
+         * exist when within quoted strings and preceded by a backslash)
+         * this\ still\"not\\allowed@example.com (even if escaped (preceded by a
+         * backslash), spaces, quotes, and backslashes must still be contained by
+         * quotes)
+         * 1234567890123456789012345678901234567890123456789012345678901234+x@example.
+         * com (local-part is longer than 64 characters)
+         * i.like.underscores@but_its_not_allowed_in_this_part (Underscore is not
+         * allowed in domain part)
+         * QA[icon]CHOCOLATE[icon]@test.com (icon characters)
+         */
         // Negative Fälle
         String[] nStringValues = { "", "?", "-", "j", "1", "ja", "nein", "Nmae", " ", " Na", "asdr", "[", "aS",
-                "wels ch@mail.xyz", "frederic-schleicher@mail.de", "y.behrendmail.com", "Lilian.kahl@yahoo.",
+                "wels ch@mail.xyz", "y.behrendmail.com", "Lilian.kahl@yahoo.",
                 "asdjsao@" };
         for (String value : nStringValues) {
             assertFalse(Validator.validateEmail(value));
